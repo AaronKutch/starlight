d675e38f8db715b241bda0b3f1d8f59022787804
Aaron Kutch <aaronkutch@att.net>
2023-12-14 15:15:17 -0600

    more high level details

--- starlight/src/route/channel.rs
+++ starlight/src/route/channel.rs
@@ -3,6 +3,7 @@ use std::cmp::Ordering;
 use awint::awint_dag::{
     smallvec::smallvec,
     triple_arena::{Arena, ArenaTrait, OrdArena},
+    EvalError,
 };
 
 use crate::{awint_dag::smallvec::SmallVec, ensemble::PBack, triple_arena::ptr_struct};
@@ -95,6 +96,80 @@ pub struct Channeler {
     backref_to_cnode: OrdArena<PBackToCNode, (PBack, PCNode), ()>,
 }
 
+// - A `CNode` cannot have exactly one subnode and must have either zero or at
+//   least two subnodes
+// - the immediate subnodes of a `CNode` must be in a clique with `CEdge`s
+
+/*
+consider a loop of `CNode`s like this
+0 --- 1
+|     |
+|     |
+2 --- 3
+
+If higher `CNode`s and edges formed like
+
+   01
+  /   \
+02    13
+  \   /
+   23
+
+It could cause an infinite loop, we need to guarantee logarithmic overhead
+with `CEdges` being made such that e.x. 02 should connect with 13 because
+02 subnodes connect with 1 and 3 which are subnodes of 13.
+
+   01
+  / | \
+02 -- 13
+  \ | /
+   23
+
+the next level is
+
+0123
+
+for larger loops it will be like
+
+0--1--2--3--4--5--6--7--0 (wraps around to 0)
+       ___   ___   ___   ___
+      /   \ /   \ /   \ /   \
+ 01-12-23-34-45-56-67-70-01-12
+   \  /  \  /  \  /  \  /
+    --    --    --    --
+
+// we do not want this to continue, or else we end up with n^2 space
+   0123  2345  4567  6701
+      1234  3456  5670  7012
+
+we notice that 12 and 23 share 0.5 of their nodes in common, what we
+do is merge a "extended clique" of cliques sharing the edge between
+the two nodes, specifically the 01-12-23 clique and the 12-23-34 clique
+
+         ...
+ 01234-45-56-67-70-01234
+
+the 01-12-23 subedges are still in the hierarchy, if the 23-34 edge is selected
+for the commonality merge, 01234 is found as a supernode of 34, and the proposed
+merge resulting in 12345 shares 12 and 23 with 01234 (if more than or equal to
+half of the subnodes are shared with respect to one or the other (2 out of
+01,12,23,34 for one or 2 out of 12,23,34,45 for the other), it should not be
+made). 34-45 would also be too close.
+45-56 however is successful resulting in 34567 which has the desired overlap.
+70 is left without a supernode on this level, but it joins a three clique to
+result in the top node
+
+       ...
+01234-34567-70-01234
+
+0123457
+
+8 -> 8 -> 3 -> 1 seems right, the first reduction is stalling for wider useful
+cliques for the descension algorithm, and this is quickly reduced down in
+the logarithmic tree we want
+
+*/
+
 impl Channeler {
     pub fn new() -> Self {
         Self {
@@ -144,15 +219,60 @@ impl Channeler {
     /// Starting from `p_cnode` assumed to contain `p_back`, this returns valid
     /// subnodes that still contain `PBack`
     pub fn valid_cnode_descensions(&self, p_cnode: PCNode, p_back: PBack) -> SmallVec<[PCNode; 4]> {
-        if let Some(p) = self
+        if let Some(p_init) = self
             .backref_to_cnode
             .find_with(|_, (p_back1, p_cnode1), ()| p_back1.cmp(&p_back))
         {
-            //
             let mut res = smallvec![];
+            //
             res
         } else {
             unreachable!()
         }
+    }
+
+    pub fn verify_integrity(&self) -> Result<(), EvalError> {
+        // verify all pointer validities first
+        for p_cnode in self.cnodes.ptrs() {
+            let cnode = &self.cnodes[p_cnode];
+            for subnode in &cnode.subnodes {
+                if !self.cnodes.contains(*subnode) {
+                    return Err(EvalError::OtherString(format!(
+                        "{cnode:?}.subnodes[{subnode}] is invalid"
+                    )))
+                }
+            }
+            for supernode in &cnode.supernodes {
+                if !self.cnodes.contains(*supernode) {
+                    return Err(EvalError::OtherString(format!(
+                        "{cnode:?}.subnodes[{supernode}] is invalid"
+                    )))
+                }
+            }
+        }
+        for p_cedge in self.cedges.ptrs() {
+            let cedge = &self.cedges.get_key(p_cedge).unwrap();
+            if !self.cnodes.contains(cedge.source) {
+                return Err(EvalError::OtherString(format!(
+                    "{cedge:?}.source {} is invalid",
+                    cedge.source
+                )))
+            }
+            if !self.cnodes.contains(cedge.sink) {
+                return Err(EvalError::OtherString(format!(
+                    "{cedge:?}.sink {} is invalid",
+                    cedge.sink
+                )))
+            }
+        }
+        for p_back_to_cnode in self.backref_to_cnode.ptrs() {
+            let (_, p_cnode) = self.backref_to_cnode.get_key(p_back_to_cnode).unwrap();
+            if !self.cnodes.contains(*p_cnode) {
+                return Err(EvalError::OtherString(format!(
+                    "{p_back_to_cnode} key {p_cnode} is invalid"
+                )))
+            }
+        }
+        Ok(())
     }
 }
