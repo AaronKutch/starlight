20a8cf0c013a59eeea80066c5f819b9faa65d794
Aaron Kutch <aaronkutch@att.net>
2023-12-29 17:34:11 -0600

    Update stats.rs

--- testcrate/tests/stats.rs
+++ testcrate/tests/stats.rs
@@ -27,3 +27,66 @@ fn stats_optimize_funnel() {
         awi::assert_eq!(ensemble.backrefs.len_vals(), 1237);
     });
 }
+
+// checks that states are being lowered and pruned at the right times and in the
+// expected amounts, and also that some optimizations are working
+#[test]
+fn stats_different_prunings() {
+    let epoch = Epoch::new();
+
+    let num_ports = 2;
+    let w = bw(1);
+    let mut net = Net::zero(w);
+    for i in 0..num_ports {
+        let mut port = awi!(0u1);
+        port.usize_(i);
+        net.push(&port).unwrap();
+    }
+    let lazy = LazyAwi::opaque(w);
+    let eval_net = EvalAwi::from(&net);
+    let res = net.drive(&lazy);
+    let eval_res = EvalAwi::from_bool(res.is_none());
+    {
+        use awi::{assert_eq, *};
+
+        epoch.ensemble(|ensemble| {
+            assert_eq!(ensemble.notary.rnodes().len(), 3);
+            assert_eq!(ensemble.stator.states.len(), 22);
+            assert_eq!(ensemble.backrefs.len_keys(), 0);
+            assert_eq!(ensemble.backrefs.len_vals(), 0);
+        });
+        epoch.lower().unwrap();
+        epoch.ensemble(|ensemble| {
+            assert_eq!(ensemble.notary.rnodes().len(), 3);
+            assert_eq!(ensemble.stator.states.len(), 19);
+            assert_eq!(ensemble.backrefs.len_keys(), 31);
+            assert_eq!(ensemble.backrefs.len_vals(), 8);
+        });
+        epoch.lower_and_prune().unwrap();
+        epoch.ensemble(|ensemble| {
+            assert_eq!(ensemble.notary.rnodes().len(), 3);
+            assert_eq!(ensemble.stator.states.len(), 0);
+            assert_eq!(ensemble.backrefs.len_keys(), 20);
+            assert_eq!(ensemble.backrefs.len_vals(), 8);
+        });
+        epoch.optimize().unwrap();
+        epoch.ensemble(|ensemble| {
+            assert_eq!(ensemble.notary.rnodes().len(), 3);
+            assert_eq!(ensemble.stator.states.len(), 0);
+            assert_eq!(ensemble.backrefs.len_keys(), 8);
+            assert_eq!(ensemble.backrefs.len_vals(), 3);
+        });
+
+        for i in 0..(1 << w.get()) {
+            let mut inx = Awi::zero(w);
+            inx.usize_(i);
+            lazy.retro_(&inx).unwrap();
+            epoch.drive_loops().unwrap();
+            awi::assert_eq!(eval_res.eval().unwrap().to_bool(), i >= num_ports);
+            if i < num_ports {
+                awi::assert_eq!(eval_net.eval().unwrap().to_usize(), i);
+            }
+        }
+        drop(epoch);
+    }
+}
